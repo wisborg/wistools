ae5fe4259caa37ee889cddcc8f019c29065fbc2f
Jesper Wisborg Krogh <wistools@wisborg.dk>
2022-02-12 16:38:29 +1100

    An initial version of the interact package

---
+++ wistools/interact.py
@@ -0,0 +1,56 @@
+import re
+
+RE_VALIDATE_ID = re.compile(r'^[1-9]\d*$')
+RE_VALIDATE_FLOAT = re.compile(r'^(?:\d+|(?:\d+)?\.(?:\d+)?)$')
+
+
+def ask(prompt: str, answer_type: str, default=None, allow_none: bool = True):
+    if default is not None:
+        question = '{0} [{1}]'.format(prompt, str(default))
+    else:
+        question = '{0}'.format(prompt)
+
+    if answer_type == 'bool':
+        question += ' (Y/N/YES/NO): '
+    else:
+        question += ': '
+
+    keep_asking = True
+    answer = None
+    while keep_asking:
+        answer = input(question)
+        if answer == '':
+            answer = str(default) if default is not None else None
+
+        valid, answer = validate(answer, answer_type, allow_none)
+        if valid:
+            keep_asking = False
+        else:
+            print('Invalid answer. Expected type: {0}'.format(answer_type))
+
+    return answer
+
+
+def validate(value, valid_type, allow_none=True):
+    valid = False
+    validated_value = None
+    if value is None:
+        valid = allow_none
+    else:
+        if valid_type == 'id':
+            if RE_VALIDATE_ID.match(value):
+                valid = True
+                validated_value = int(value)
+        elif valid_type == 'bool':
+            valid = value.upper() in ('Y', 'YES', 'N', 'NO')
+            if valid:
+                validated_value = value.upper() in ('Y', 'YES')
+        elif valid_type == 'float':
+            if RE_VALIDATE_FLOAT.match(value):
+                valid = True
+                validated_value = float(value)
+        elif valid_type == 'string':
+            valid = True
+            validated_value = str(value)
+
+    return valid, validated_value
