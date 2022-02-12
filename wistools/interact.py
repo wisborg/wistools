40c5500fa0d18788109ab991b92441c2c95b2119
Jesper Wisborg Krogh <wistools@wisborg.dk>
2022-02-12 17:09:36 +1100

    Add multiline support to interact.ask()

--- wistools/interact.py
+++ wistools/interact.py
@@ -4,7 +4,8 @@ RE_VALIDATE_ID = re.compile(r'^[1-9]\d*$')
 RE_VALIDATE_FLOAT = re.compile(r'^(?:\d+|(?:\d+)?\.(?:\d+)?)$')
 
 
-def ask(prompt: str, answer_type: str, default=None, allow_none: bool = True):
+def ask(prompt: str, answer_type: str, default=None, allow_none: bool = True,
+        multiline: bool = False):
     if default is not None:
         question = '{0} [{1}]'.format(prompt, str(default))
     else:
@@ -12,13 +13,27 @@ def ask(prompt: str, answer_type: str, default=None, allow_none: bool = True):
 
     if answer_type == 'bool':
         question += ' (Y/N/YES/NO): '
+    elif multiline:
+        question += ' (empty line or EOF to stop)'
     else:
         question += ': '
 
     keep_asking = True
     answer = None
+    paragraphs = []
     while keep_asking:
-        answer = input(question)
+        if multiline:
+            try:
+                answer = input().strip()
+            except EOFError:
+                answer = ''
+            if answer:
+                paragraphs.append(answer)
+                print('\n')
+            else:
+                answer = '\n\n'.join(paragraphs)
+        else:
+            answer = input(question)
         if answer == '':
             answer = str(default) if default is not None else None
 
