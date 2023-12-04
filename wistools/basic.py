bc8247281c205072b67d10a8c6b5b5b8f778dea9
Jesper Wisborg Krogh <wistools@wisborg.dk>
2023-12-04 11:31:41 +1100

    Add function to round decimals

--- wistools/basic.py
+++ wistools/basic.py
@@ -2,6 +2,8 @@
 Various basic utilities such as working with iterables
 """
 
+from decimal import Decimal, ROUND_HALF_UP
+
 
 def iterate(items):
     """Allow iterating over anything. Strings and bytes are considered
@@ -15,3 +17,8 @@ def iterate(items):
         yield from items
     except TypeError:
         yield items
+
+
+def round_decimal(value: Decimal, fmt: str) -> Decimal:
+    """Round decimal values using the ROUND_HALF_UP rule."""
+    return value.quantize(Decimal(fmt), rounding=ROUND_HALF_UP)
